8089c795f90f503cb6be0d9f0cb0e086f33ac864
Sidroded <deinekindaniil21@gmail.com>
2023-11-21 15:54:03 +0100

    Add Schedule

--- src/main/java/com/sidroded/Main.java
+++ src/main/java/com/sidroded/Main.java
@@ -3,13 +3,21 @@ package com.sidroded;
 import com.sidroded.service.ConsoleService;
 import com.sidroded.service.FileService;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     private static final FileService fileService = new FileService();
     private static final ConsoleService consoleService = new ConsoleService();
     public static void main(String[] args) {
-        fileService.createFile();
-        fileService.updateFile();
-        consoleService.gitCommit();
-        consoleService.gitPush();
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+
+        executor.scheduleAtFixedRate(() -> {
+            fileService.createFile();
+            fileService.updateFile();
+            consoleService.gitCommit();
+            consoleService.gitPush();
+        }, 0, 1, TimeUnit.MINUTES);
     }
 }
